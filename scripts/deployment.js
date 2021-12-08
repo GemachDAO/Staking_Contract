bbaf405752f8d98d83c9e870634c9d5b5754fca6
Dontrail Cotlage <kingassune@gmail.com>
2021-12-08 14:19:42 -0500

    configured staking contract with deployment script

---
+++ scripts/deployment.js
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  
+  const tokenAddress = "x000000000000000000000000000000000000000";
+
+  const startRewards = 0;
+  
+  const Rewards = await hre.ethers.getContractFactory("Rewards");
+  const rewards = await Rewards.deploy(tokenAddress, _startRewards);
+
+  await rewards.deployed();
+
+  console.log("Stalking Reward Contract has been deployed to this Address: ", rewards.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
